282020f2bd5fb653f2fa5c048e3f74ecf05b4aa1
Pedro Pires <opdpires@ufmg.br>
2022-10-31 18:30:58 -0300

    Create the project for the API and include the header and source files

---
+++ API_BLUETOOTH_F446RE/Core/Inc/Bluetooth_BLE_V4.2_JDY-18.h
@@ -0,0 +1,24 @@
+/**
+ *      Data: 31/10/2022
+ *      Universidade
+ *      Autores: Pedro Otávio Fonseca Pires e Leandro Guatimosim
+ *      Versão: 1.0
+ *      Licença: MIT
+ *      Nome da API: API para usode um Módulo Bluetooth BLE V4.2 JDY-18
+ *      Arquivos relacionados: Bluetooth_BLE_V4.2_JDY-18.h
+ *      					   Bluetooth_BLE_V4.2_JDY-18.c
+ *      Resumo: Esta API foi desenvolvida como trabalho da disciplina de Programação de Sistemas Embarcados da UFMG
+ *             – Prof. Ricardo de Oliveira Duarte – Departamento de Engenharia Eletrônica
+ *------------------------------------------------------------------------------------------------------------------------------------------------------
+ *      @file Bluetooth_BLE_V4.2_JDY-18.h
+ *      @author Pedro Otávio Fonseca Pires and Leandro Guatimosim Gripp
+ *      @brief This is the header file for the API of the Bluetooth_BLE_V4.2_JDY-18 module.
+ *      	   Datasheet: https://manuals.plus/bluetooth-module/bluetooth-jdy-18-4-2-ble-module-usage-manual#axzz7jKHPWZQl
+ */
+
+#ifndef INC_BLUETOOTH_BLE_V4_2_JDY_18_H_
+#define INC_BLUETOOTH_BLE_V4_2_JDY_18_H_
+
+
+
+#endif /* INC_BLUETOOTH_BLE_V4_2_JDY_18_H_ */
